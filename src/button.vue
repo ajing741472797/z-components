c2a486cf90455f3567adbbbb1b56ad1d52b26601
vic <741472797@qq.com>
2019-07-17 10:54:36 +0800

    添加icon 和 icon-position

--- src/button.vue
+++ src/button.vue
@@ -1,11 +1,25 @@
 <template>
-    <button class="z-button">点我</button>
+    <button class="z-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+        <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
 export default {
+    // props: ['icon','iconPosition']//写法一
+    props:{
+        icon:{},
+        iconPosition:{
+            type:String,
+            default:'left' //默认值left
+        }
 
-    
+    }//写法
 }
 </script>
 
@@ -17,6 +31,10 @@ export default {
             border-radius: var(--border-radius);
             border: 1px solid var(--border-color);
             background: var(--button-bg);
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            vertical-align: middle; // css遇到上下不对齐的情况，更改这个属性可以解决
 
         &:hover{
             border-color: var(--border-color-hover)
@@ -27,8 +45,25 @@ export default {
         &:focus{
             outline: none
         }
+        > .content{
+            order: 2
+        }
+        > .icon{
+            margin-right: .1em;
+            order: 1
         }
 
+        &.icon-right{
+            > .content{
+                order: 1
+            }
+            > .icon{
+                margin-right: 0;
+                margin-left: .1em;
+                order:2
+            }
+        }
+        }
 
 </style>
 
