4cbbf39288cd99f055b56c62aa9cd19e9caae157
vic <741472797@qq.com>
2019-08-21 20:49:15 +0800

    让 input 支持 v-model 双向绑定

--- test/input.test.js
+++ test/input.test.js
@@ -74,9 +74,14 @@ import { eventNames } from 'cluster';
             vm.$on(eventName,callback)
             //触发input的 change 事件
            let event = new Event(eventName);
+           Object.defineProperty(
+             event, 'target',{
+               value:{value: 'hi'},enumerable: true
+             }
+           )
            let inputElement = vm.$el.querySelector('input')
            inputElement.dispatchEvent(event)
-           expect(callback).to.have.been.calledWith(event)  
+           expect(callback).to.have.been.calledWith('hi')  
 
           })
 
