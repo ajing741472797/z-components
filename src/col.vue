8b7e9099fa131be5aaad3c8e1402990dd1df8999
vic <741472797@qq.com>
2019-08-29 15:28:01 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -1,62 +1,146 @@
 <template>
-  <div class="col" :class="colClass"
-  :style="colStyle">
+  <div class="col"
+       :class="colClass"
+       :style="colStyle">
     <div style="height:100px">
-    <slot></slot>
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script>
+let validator = (value) => {
+  let keys = Object.keys(value)
+  let valid = true
+  keys.forEach(key => {
+    if (!['span', 'offset'].includes(key)) {
+      valid = false
+    }
+  })
+  return valid
+}
+
+
 export default {
   name: 'GluluCol',
-  props:{
-    span:{
-      type:[Number, String]
-      },
-    offset:{
-      type:[Number, String]
-      },
-
+  props: {
+    span: { type: [Number, String]},
+    offset: {type: [Number, String]},
+    ipad: {type: Object,validator},
+    narrowPc: {type: Object,validator},
+    pc: {type: Object,validator},
+    widePc: {type: Object,validator}
   },
-  data (){
+  data () {
     return {
-    gutter: 0
-  }
+      gutter: 0
+    }
   },
   computed: {
-    colClass(){
-      let {span, offset} = this
-      return [span && `col-${span}`,offset && `offset-${offset}`]
+    colClass () {
+      let { span, offset, ipad, narrowPc, pc, widePc} = this
+
+      return [
+        span && `col-${span}`,
+      offset && `offset-${offset}`,
+      ... (ipad ? [`col-ipad-${ipad.span}`] : []),
+      ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+      ... (pc ? [`col-pc-${pc.span}`] : []),
+      ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
+      ]
     },
-    colStyle (){
-      return{
-        paddingLeft: this.gutter/2 + 'px', 
-        paddingRight: this.gutter/2 + 'px',
-        }
+    colStyle () {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px',
+      }
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-    .col{
-        height: 100px;
-        // background: gray;
-        // border: 1px solid red;
-        padding: 0 10px;
-        // for循环
-        $class-prefix: col-;
-        @for $n from 1 through 24{
-          &.#{$class-prefix}#{$n}{
-            width: ($n / 24) * 100%;
-          }
-        }
+.col {
+  height: 100px;
+  // background: gray;
+  // border: 1px solid red;
+  padding: 0 10px;
+  // for循环
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
 
-         $class-prefix: offset-;
-        @for $n from 1 through 24{
-          &.#{$class-prefix}#{$n}{
-           margin-left: ($n / 24) * 100%;
-          }
-        }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
+    }
+  }
+ 
+  @media (min-width: 577px) {
+    $class-prefix: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
       }
+    }
+
+    $class-prefix: offset-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 769px){
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 993px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 1201px) {
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+}
 </style>
