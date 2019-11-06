8b92f9e4b8f081db018d951534be0365ac464f2d
vic <741472797@qq.com>
2019-11-06 15:28:58 +0800

    增大tab item可点击区域

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -46,6 +46,11 @@ computed:{
   .tabs-item{
     flex-shrink: 0;
     padding: 0 1em;
+    border: 1px solid red;
+    cursor: pointer;
+    height: 100%;
+    display: flex;
+    align-items: center;
     &.active{
       background: red;
     }
