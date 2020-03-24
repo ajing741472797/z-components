0d4d9e6d78442660bccbc3c5b7803302a2c89134
vic <741472797@qq.com>
2020-03-25 00:31:14 +0800

    两种方式触发

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="popover"
-       @click="onClick"
        ref="popover">
     <div ref="contentWrapper"
          class="content-wrapper"
@@ -21,7 +20,41 @@ export default {
   name: 'GuluPopover',
   data () {
     return {
-      visible: false
+      visible: false,
+
+    }
+  },
+  mounted () {
+    if (this.triggerWrapper === 'click') {
+      this.$refs.popover.addEventListener('click', this.onClick)
+    } else {
+      this.$refs.popover.addEventListener('mouseenter', this.open)
+      this.$refs.popover.addEventListener('mouseleave', this.close)
+    }
+  },
+  destroyed () {
+    if (this.triggerWrapper === 'click') {
+      this.$refs.popover.removeEventListener('click', this.onClick)
+    } else {
+      this.$refs.popover.removeEventListener('mouseenter', this.open)
+      this.$refs.popover.removeEventListener('mouseleave', this.close)
+    }
+  },
+  computed: {
+    openEvent () {
+      if (this.trigger === 'click') {
+        return 'click'
+      } else {
+        return 'mouseenter'
+      }
+    },
+    closeEvent () {
+      if (this.trigger === 'click') {
+        return 'click'
+      } else {
+        return 'mouseleave'
+      }
+
     }
   },
   props: {
@@ -30,6 +63,13 @@ export default {
       default: 'top',
       validator (value) {
         return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+      }
+    },
+    trigger: {
+      type: String,
+      default: 'click',
+      validator (value) {
+        return ['click', 'hover'].indexOf(value) >= 0
       }
     }
   },
@@ -62,8 +102,8 @@ export default {
             (height - height2) / 2
         },
       }
-        contentWrapper.style.left = positions[this.position].left + 'px'
-        contentWrapper.style.top =  positions[this.position].top + 'px'
+      contentWrapper.style.left = positions[this.position].left + 'px'
+      contentWrapper.style.top = positions[this.position].top + 'px'
     },
     onClickDocument (e) {
       if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
@@ -84,19 +124,8 @@ export default {
       this.visible = false
       document.removeEventListener('click', this.onClickDocument)
     },
-    onClick (event) {
-      if (this.$refs.triggerWrapper.contains(event.target)) {
-        if (this.visible === true) {
-          this.close()
-        } else {
-          this.open()
-        }
-      }
-    },
 
   },
-  mounted () {
-  }
 }
 </script>
 
